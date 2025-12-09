Hanineee01/Systeme-d-alertes

Toast qui marche mais pas pour les allertes

ClientAlertesWPF/Services/AlertServices.cs
@@ -1,12 +1,13 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
 using ClientAlertesWPF.ViewModels;
 using Microsoft.AspNetCore.SignalR.Client;
-using CommunityToolkit.WinUI.Notifications; // <-- Nouveau package
+using CommunityToolkit.WinUI.Notifications;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -24,15 +25,22 @@ namespace ClientAlertesWPF.Services
         {
             _tb = tb;
             _logoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Icons", "logo.png");
+
+            // Affiche l'ID dans le tooltip pour debug
+            _tb.ToolTipText = $"Système d'alertes (ID: {_machineId})";
         }
 
         public async void Start()
         {
+            // 1. Enregistrement de la machine sur le serveur (obligatoire)
+            await RegisterMachineAsync();
+
+            // 2. SignalR (temps réel)
             _connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5000/alerthub")
+                .WithUrl("http://localhost:5000/alerthub") // Change le port si ton API est sur un autre
                 .Build();
 
-            _connection.On<string>("ReceiveAlert", json =>
+            _connection.On<string>("ReceiveAlert", (json) =>
             {
                 var vm = JsonConvert.DeserializeObject<AlertViewModel>(json);
                 if (vm != null) ShowToast(vm);
@@ -41,48 +49,84 @@ namespace ClientAlertesWPF.Services
             try
             {
                 await _connection.StartAsync();
-                _tb.ToolTipText = "Alertes : Temps réel OK";
+                _tb.ToolTipText += " - Connecté en temps réel";
             }
-            catch
+            catch (Exception)
             {
-                _ = PollingLoop();
-                _tb.ToolTipText = "Alertes : Polling (5s)";
+                _tb.ToolTipText += " - Mode polling activé";
+                _ = PollingLoopAsync();
             }
         }
 
-        private async Task PollingLoop()
+        private async Task RegisterMachineAsync()
+        {
+            try
+            {
+                var payload = new
+                {
+                    UniqueId = _machineId.ToString(),
+                    Name = Environment.MachineName
+                };
+
+                var json = JsonConvert.SerializeObject(payload);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                await _http.PostAsync("http://localhost:5000/api/machines/register", content);
+            }
+            catch { }
+        }
+
+        private async Task PollingLoopAsync()
         {
             while (true)
             {
                 try
                 {
                     var response = await _http.GetAsync($"http://localhost:5000/api/alerts/pending/{_machineId}");
+
                     if (response.IsSuccessStatusCode)
                     {
                         var json = await response.Content.ReadAsStringAsync();
                         var alerts = JsonConvert.DeserializeObject<List<AlertViewModel>>(json);
-                        foreach (var vm in alerts ?? new List<AlertViewModel>())
-                            ShowToast(vm);
+
+                        if (alerts != null)
+                        {
+                            foreach (var vm in alerts)
+                            {
+                                ShowToast(vm);
+                            }
+                        }
                     }
                 }
                 catch { }
 
-                await Task.Delay(5000);
+                await Task.Delay(4000); // 4 secondes
             }
         }
 
         private void ShowToast(AlertViewModel vm)
         {
-            new ToastContentBuilder()
+            var toast = new ToastContentBuilder()
                 .AddAppLogoOverride(new Uri(_logoPath), ToastGenericAppLogoCrop.Circle)
-                .AddHeader(vm.Level, vm.Title, "")
-                .AddText(vm.Message)
-                .AddButton(new ToastButton("OK", "ack").SetBackgroundActivation())
-                .SetToastScenario(vm.Level == "Critical" ? ToastScenario.Alarm : ToastScenario.Default)
-                .AddAudio(vm.Level == "Critical"
-                    ? new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Looping.Alarm"), Loop = true }
-                    : new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Default") })
-                .Show();
+                .AddHeader(vm.Level ?? "Info", vm.Title ?? "Alerte", "")
+                .AddText(vm.Message ?? "")
+                .AddButton(new ToastButton("OK", "dismiss").SetBackgroundActivation());
+
+            if (vm.Level == "Critical")
+            {
+                toast.AddAudio(new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Looping.Alarm"), Loop = true });
+                toast.SetToastScenario(ToastScenario.Alarm);
+            }
+            else if (vm.Level == "Warning")
+            {
+                toast.AddAudio(new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.IM") });
+            }
+            else
+            {
+                toast.AddAudio(new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Default") });
+            }
+
+            toast.Show();
         }
 
         public void Stop()

Hanineee01/Syst-me-d-alertes

ClientAlertesWPF/Services/AlertServices.cs
@@ -1,12 +1,13 @@
 ﻿using Hardcodet.Wpf.TaskbarNotification;
 using ClientAlertesWPF.ViewModels;
 using Microsoft.AspNetCore.SignalR.Client;
-using CommunityToolkit.WinUI.Notifications; // <-- Nouveau package
+using CommunityToolkit.WinUI.Notifications;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -24,15 +25,18 @@ namespace ClientAlertesWPF.Services
         {
             _tb = tb;
             _logoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Icons", "logo.png");
+            _tb.ToolTipText = $"Système d'alertes (ID: {_machineId})";
         }
 
         public async void Start()
         {
+            await RegisterMachineAsync();
+
             _connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5000/alerthub")
+                .WithUrl("http://localhost:5177/hubs/alertes") // Matcher l'API
                 .Build();
 
-            _connection.On<string>("ReceiveAlert", json =>
+            _connection.On<string>("ReceiveAlert", (json) =>
             {
                 var vm = JsonConvert.DeserializeObject<AlertViewModel>(json);
                 if (vm != null) ShowToast(vm);
@@ -41,16 +45,35 @@ namespace ClientAlertesWPF.Services
             try
             {
                 await _connection.StartAsync();
-                _tb.ToolTipText = "Alertes : Temps réel OK";
+                _tb.ToolTipText += " - SignalR connecté";
             }
             catch
             {
-                _ = PollingLoop();
-                _tb.ToolTipText = "Alertes : Polling (5s)";
+                _tb.ToolTipText += " - Mode polling";
+                _ = PollingLoopAsync();
             }
         }
 
-        private async Task PollingLoop()
+        private async Task RegisterMachineAsync()
+        {
+            try
+            {
+                var payload = new
+                {
+                    TokenUnique = _machineId.ToString(),
+                    Nom = Environment.MachineName,
+                    DerniereConnexion = DateTime.UtcNow
+                };
+
+                var json = JsonConvert.SerializeObject(payload);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                await _http.PostAsync("http://localhost:5000/api/machines/register", content);
+            }
+            catch { }
+        }
+
+        private async Task PollingLoopAsync()
         {
             while (true)
             {
@@ -61,8 +84,13 @@ namespace ClientAlertesWPF.Services
                     {
                         var json = await response.Content.ReadAsStringAsync();
                         var alerts = JsonConvert.DeserializeObject<List<AlertViewModel>>(json);
-                        foreach (var vm in alerts ?? new List<AlertViewModel>())
-                            ShowToast(vm);
+                        if (alerts != null)
+                        {
+                            foreach (var vm in alerts)
+                            {
+                                ShowToast(vm);
+                            }
+                        }
                     }
                 }
                 catch { }
@@ -73,16 +101,31 @@ namespace ClientAlertesWPF.Services
 
         private void ShowToast(AlertViewModel vm)
         {
-            new ToastContentBuilder()
+            var builder = new ToastContentBuilder()
                 .AddAppLogoOverride(new Uri(_logoPath), ToastGenericAppLogoCrop.Circle)
-                .AddHeader(vm.Level, vm.Title, "")
-                .AddText(vm.Message)
-                .AddButton(new ToastButton("OK", "ack").SetBackgroundActivation())
-                .SetToastScenario(vm.Level == "Critical" ? ToastScenario.Alarm : ToastScenario.Default)
-                .AddAudio(vm.Level == "Critical"
-                    ? new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Looping.Alarm"), Loop = true }
-                    : new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Default") })
-                .Show();
+                .AddHeader(vm.Level ?? "Info", vm.Title ?? "Alerte", "")
+                .AddText(vm.Message ?? "")
+                .AddButton(new ToastButton("OK", "dismiss").SetBackgroundActivation());
+
+            if (vm.Level == "Critical")
+            {
+                builder.AddAudio(new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Looping.Alarm"), Loop = true });
+                builder.SetToastScenario(ToastScenario.Alarm);
+            }
+            else if (vm.Level == "Warning")
+            {
+                builder.AddAudio(new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.IM") });
+            }
+            else
+            {
+                builder.AddAudio(new ToastAudio { Src = new Uri("ms-winsoundevent:Notification.Default") });
+            }
+
+            // === AJOUTE ÇA ICI : pour que la toast expire après 5 minutes ===
+            builder.Show(toast =>
+            {
+                toast.ExpirationTime = DateTime.Now.AddMinutes(5);
+            });
         }
 
         public void Stop()